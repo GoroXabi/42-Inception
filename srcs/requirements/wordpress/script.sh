c0c0f05d26fdb30a049b89b2aa79b20ce8b2ba11
Xabier <xaberortega@gmail.com>
2025-05-15 14:13:47 +0200

    missing secrets and var

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+
+sleep 3
+if ! [ -d /var/www/html/index.php ]; then
+echo DOWLOADING WORDPRESS
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
-
+fi
 php-fpm83 -F
